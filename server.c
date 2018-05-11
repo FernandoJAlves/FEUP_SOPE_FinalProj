96f347fb6c714427a7942a183cb7fb03484d2db4
FernandoJAlves <up201605270@fe.up.pt>
2018-05-11 20:12:05 +0100

    Continued working on server main func

--- server.c
+++ server.c
@@ -19,6 +19,26 @@ int main(int argc, char *argv[]) {
 
   sleep(1);
 
+  int num_room_seats = argv[1];
+  int num_ticket_offices = argv[2];
+  int open_time = argv[3];
+
+
+  int start_time = 0; // use C function to get the start time of the program
+
+  while(!time_is_over(start_time, open_time)){
+
+  	//All the code will be in here
+
+  	
+
+
+
+  }
+
+
+
+
   return 0;
 }
 
@@ -30,3 +50,9 @@ int initFIFOs(){
 }
 
 
+bool time_is_over(int start_time, int interval){
+
+	//use the C function to get current time and see if the dif between that and start_time is greater than the given interval (in seconds)
+
+	return false;
+}
